ae2bf3c71ab9f3057ea2116fad9575a17713f09f
cwal37 <cwal37@gmail.com>
2016-03-26 19:00:41 -0400

    code sans graphing finished

--- weighted_capacity_age_graph.py
+++ weighted_capacity_age_graph.py
@@ -18,15 +18,39 @@ years = np.array(list(set(current_gen['Operating Year'])))
 sort_idx = years.argsort()
 years = years[sort_idx]
 
-graph_data = ['Year', 'Technology', 'Prime Mover' 
+graph_data = [] 
+graph_data.append(['Year', 'Technology', 'Prime Mover', 'Capacity', 'Fraction'])
 
 for year in years:
-    year_gens = current_gen[current_gen['Operating Year'] == year]
-    
-    prime_movers = list(set(year_gens['Prime Mover']))
-    technologies = list(set(year_gens['Technology']))
     
     running_plants = current_gen[current_gen['Operating Year'] <= year]
-    total_cap = np.sum(running_plants['Nameplate Capacity (MW)'])
     
+    prime_movers = list(set(running_plants['Prime Mover']))
+    technologies = list(set(running_plants['Technology']))
+    
+    total_cap = np.sum(pd.to_numeric(pd.Series(running_plants['Nameplate Capacity (MW)']), errors='coerce'))
+    
+    for tech in technologies:
+        tech_gen = running_plants[running_plants['Technology'] == tech]
+        tech_gen_MW = pd.to_numeric(pd.Series(tech_gen['Nameplate Capacity (MW)']), errors='coerce')
+        tech_gen_total = np.sum(tech_gen_MW)
+        tech_gen_fraction = tech_gen_total/total_cap
+        graph_data.append([year, tech, 'NaN', tech_gen_total, tech_gen_fraction])
+        
+    for PM in prime_movers:
+        tech_gen = running_plants[running_plants['Prime Mover'] == PM]
+        tech_gen_MW = pd.to_numeric(pd.Series(tech_gen['Nameplate Capacity (MW)']), errors='coerce')
+        tech_gen_total = np.sum(tech_gen_MW)
+        tech_gen_fraction = tech_gen_total/total_cap
+        graph_data.append([year, 'NaN', PM, tech_gen_total, tech_gen_fraction])
+        
+        
+
+graphing_data = pd.DataFrame(columns=graph_data[0])      
+      
+for i in range(1,len(graph_data)):
+    graphing_data.loc[i] = graph_data[i]
+    
+    
+        
     
