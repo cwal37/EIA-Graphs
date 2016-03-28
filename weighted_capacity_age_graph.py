7e28503d5190a516a94a555308b2fde9956a5cab
CJW <cwal37@gmail.com>
2016-03-27 22:33:37 -0400

    Last save
    Forgot to save before updating.

--- weighted_capacity_age_graph.py
+++ weighted_capacity_age_graph.py
@@ -22,49 +22,50 @@ mpl.rcParams.update({'font.size': 15})
 # Code to bring in data and generate a csv to save graphing data somewhere so I
 # don't need to run this code every time, and I have a "physical" file to inspect.
 
-##future_gen = pd.read_csv('gen_future_Y2014.csv', skiprows = 1)
-#current_gen = pd.read_csv('gen_exist_Y2014.csv', skiprows = 1, low_memory = False)
-#
-#years = np.array(list(set(current_gen['Operating Year'])))
-#
-#
-#sort_idx = years.argsort()
-#years = years[sort_idx]
-#
-#graph_data = [] 
-#graph_data.append(['Year', 'Technology', 'Prime Mover', 'Capacity', 'Fraction'])
-#
-#prime_movers = list(set(current_gen['Prime Mover']))
-#technologies = list(set(current_gen['Technology']))
-#
-#for year in years:
-#    
-#    running_plants = current_gen[current_gen['Operating Year'] <= year]
-#    
-#
-#    
-#    total_cap = np.sum(pd.to_numeric(pd.Series(running_plants['Nameplate Capacity (MW)']), errors='coerce'))
-#    
-#    for tech in technologies:
-#        tech_gen = running_plants[running_plants['Technology'] == tech]
-#        tech_gen_MW = pd.to_numeric(pd.Series(tech_gen['Nameplate Capacity (MW)']), errors='coerce')
-#        tech_gen_total = np.sum(tech_gen_MW)
-#        tech_gen_fraction = tech_gen_total/total_cap
-#        graph_data.append([year, tech, 'NaN', tech_gen_total, tech_gen_fraction])
-#        
-#    for PM in prime_movers:
-#        tech_gen = running_plants[running_plants['Prime Mover'] == PM]
-#        tech_gen_MW = pd.to_numeric(pd.Series(tech_gen['Nameplate Capacity (MW)']), errors='coerce')
-#        tech_gen_total = np.sum(tech_gen_MW)
-#        tech_gen_fraction = tech_gen_total/total_cap
-#        graph_data.append([year, 'NaN', PM, tech_gen_total, tech_gen_fraction])
-#        
-#        
-#
-#graphing_data = pd.DataFrame(columns=graph_data[0])      
-#      
-#for i in range(1,len(graph_data)):
-#    graphing_data.loc[i] = graph_data[i]
-#    
-##graphing_data.to_csv('graphing_data.csv')
-#print 'done'
+#future_gen = pd.read_csv('gen_future_Y2014.csv', skiprows = 1)
+current_gen = pd.read_csv('gen_exist_Y2014.csv', skiprows = 1, low_memory = False)
+
+years = np.array(list(set(current_gen['Operating Year'])))
+
+
+sort_idx = years.argsort()
+years = years[sort_idx]
+
+graph_data = [] 
+graph_data.append(['Year', 'Technology', 'Prime Mover', 'Capacity', 'Fraction'])
+
+prime_movers = list(set(current_gen['Prime Mover']))
+technologies = list(set(current_gen['Technology']))
+
+for year in years:
+    
+    running_plants = current_gen[current_gen['Operating Year'] <= year]
+    current_years  = 
+    
+
+    
+    total_cap = np.sum(pd.to_numeric(pd.Series(running_plants['Nameplate Capacity (MW)']), errors='coerce'))
+    
+    for tech in technologies:
+        tech_gen = running_plants[running_plants['Technology'] == tech]
+        tech_gen_MW = pd.to_numeric(pd.Series(tech_gen['Nameplate Capacity (MW)']), errors='coerce')
+        tech_gen_total = np.sum(tech_gen_MW)
+        tech_gen_fraction = tech_gen_total/total_cap
+        graph_data.append([year, tech, 'NaN', tech_gen_total, tech_gen_fraction])
+        
+    for PM in prime_movers:
+        tech_gen = running_plants[running_plants['Prime Mover'] == PM]
+        tech_gen_MW = pd.to_numeric(pd.Series(tech_gen['Nameplate Capacity (MW)']), errors='coerce')
+        tech_gen_total = np.sum(tech_gen_MW)
+        tech_gen_fraction = tech_gen_total/total_cap
+        graph_data.append([year, 'NaN', PM, tech_gen_total, tech_gen_fraction])
+        
+        
+
+graphing_data = pd.DataFrame(columns=graph_data[0])      
+      
+for i in range(1,len(graph_data)):
+    graphing_data.loc[i] = graph_data[i]
+    
+#graphing_data.to_csv('graphing_data.csv')
+print 'done'
